ebd1508942103ae8214adc9389f7805166840745
Camilo <cadiazb@berkeley.edu>
2015-04-06 08:30:35 -0700

    Added frequency of reward is controlled by amount of force, so the harder MP pulls the higher the frequency juice is delivered

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -201,7 +201,7 @@ b5.Cursor_scale 				= [0 0];        % [mm] % note: diameter!
 %% Start Target
 b5.StartTarget_color			= [1 0 0 1];
 b5.StartTarget_scale 			= [20 20];
-Params.StartTarget.Win  		= [70 50]; % radius
+Params.StartTarget.Win  		= [40 60]; % radius
 Params.StartTarget.Locations 	= {Params.WsCenter + [-40 -40]}; % cell array of locations
 
 %% Rewards
