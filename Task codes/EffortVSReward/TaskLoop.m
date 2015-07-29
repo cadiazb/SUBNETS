b8ddadf96a83ab121e4a332946c227f7f97a66be
Camilo <cadiazb@berkeley.edu>
2015-07-29 07:26:58 -0700

    Fixed adaptive sampling by giving a better initial model, and added a try statement to prevent task from crashing

--- Task codes/EffortVSReward/TaskLoop.m
+++ Task codes/EffortVSReward/TaskLoop.m
@@ -293,6 +293,7 @@ for itrial = startTrial : Params.NumTrials
     end
 
     %% calculate the reward based on the short-term performance
+    try
         if sum([Data.OutcomeID] == 0) > 30
             % TopReward = BottomReward * Params.BiasingMulti
             tmpIdx = find([Data.OutcomeID]==0,20,'last');
@@ -301,9 +302,10 @@ for itrial = startTrial : Params.NumTrials
                                 [Data(tmpIdx).TrialChoiceID], ...
                                     Params.RewardModel);
                                 
-            Params.BiasingMulti = tmpReward / Data(itrial).ProbeReward;
+            Params.BiasingMulti = min(5e3, max(100, tmpReward)) / Data(itrial).ProbeReward;
 
         end
+    end
     
     %% Save Data
     if QUIT_FLAG
--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -219,8 +219,8 @@ Params.RewardsVector        = 200; %[ms]
 Params.BiasingMulti         = 0.5;
 
 %Model for adaptation of reward
-Params.RewardModel.xo          = 105;
-Params.RewardModel.w            = 0.16;
+Params.RewardModel.xo          = 75;
+Params.RewardModel.w            = 1.5e3;
 
 %% Effort
 Params.LoadCellMax                  = 50;
