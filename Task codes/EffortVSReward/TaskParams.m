50e69f885d74f220d1896d40df567f09634d9598
Camilo <cadiazb@berkeley.edu>
2015-11-12 15:27:30 -0800

    increased bottom hold time

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -138,7 +138,7 @@ Params.TimeoutReachStartTarget  = 2; % max time to acquire start target
 Params.StartTarget.Hold       	= 0.6; %0.5
 
 % Reaching phase
-Params.HoldDown                 = 0.2; % required min hold time
+Params.HoldDown                 = 0.5; % required min hold time
 Params.HoldUp                   = 0.1;
 Params.ReactionTimeDelay      	= 2; % Max time to initiate movement
 
@@ -193,7 +193,7 @@ Params.UpTargetProbability      = 0.5; % for joystickTraining mode
 % Rewards
 Params.StdReward                = 100; %[ms]
 % multipliers for StdReward
-Params.UpReward                 = [0.9 0.8 0.9 0.6, rand(1,100)*0.8+0.1]; 
+Params.UpReward                 = [0.9 0.9 0.8 0.9 0.6 0.7 0.8 0.9 0.9, rand(1,100)*0.8+0.1]; 
 Params.DownReward               = 1.0-Params.UpReward;
 Params.UpReward=2*Params.UpReward;
 Params.DownReward=2*Params.DownReward;
