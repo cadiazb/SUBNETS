13d9db797d921998300f89875e4b3b91a4c4e6d9
Camilo <cadiazb@berkeley.edu>
2015-11-20 10:24:47 -0800

    removed bais to up at start

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -193,7 +193,7 @@ Params.UpTargetProbability      = 0.5; % for joystickTraining mode
 % Rewards
 Params.StdReward                = 150; %[ms]
 % multipliers for StdReward
-Params.UpReward                 = [0.9 0.9 0.7 0.5 rand(1,100)*0.8+0.1]; 
+Params.UpReward                 = [0.7 0.5 rand(1,100)*0.8+0.1]; 
 Params.DownReward               = 1.0-Params.UpReward;
 Params.UpReward=2*Params.UpReward;
 Params.DownReward=2*Params.DownReward;
