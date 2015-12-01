069a4db12db389c1d80922643aa06aa0041c815b
Camilo <cadiazb@berkeley.edu>
2015-11-30 18:18:21 -0800

    changed bias blocks at start

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -192,7 +192,7 @@ Params.UpTargetProbability      = 0.5; % for joystickTraining mode
 % Rewards
 Params.StdReward                = 180; %[ms]
 % multipliers for StdReward
-Params.UpReward                 = [0.9 0.8 0.5 0.8 rand(1,100)*0.8+0.1]; 
+Params.UpReward                 = [0.9 0.5 0.1 0.9 rand(1,100)*0.8+0.1]; 
 Params.DownReward               = 1.0-Params.UpReward;
 Params.UpReward=2*Params.UpReward;
 Params.DownReward=2*Params.DownReward;
