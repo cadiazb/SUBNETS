267762702ec78aeff13ce1c983befa0808d869fb
Camilo <cadiazb@berkeley.edu>
2015-12-07 15:48:47 -0800

    reduced stdR

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -190,7 +190,7 @@ Params.DownTarget_pos           = Params.StartTarget_pos + ...
 Params.UpTargetProbability      = 0.5; % for joystickTraining mode
 
 % Rewards
-Params.StdReward                = 150; %[ms]
+Params.StdReward                = 120; %[ms]
 % multipliers for StdReward
 Params.UpReward                 = [0.9 0.8 0.7 0.9 0.7 0.8 0.6 rand(1,100)*0.8+0.1]; 
 Params.DownReward               = 1.0-Params.UpReward;
