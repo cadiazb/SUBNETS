965ccd0a53c1678ff63234c1b0e5d335dfd38496
kderosier <kderosier6@gmail.com>
2015-12-16 14:29:29 -0800

    increased down hold, added up R bias blocks;

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -140,7 +140,7 @@ Params.TimeoutReachStartTarget  = 2.5; % max time to acquire start target
 Params.StartTarget.Hold       	= 0.6; %0.5
 
 % Reaching phase
-Params.HoldDown                 = 0.65; % required min hold time
+Params.HoldDown                 = 0.7; % required min hold time
 Params.HoldUp                   = 0.35;
 Params.ReactionTimeDelay      	= 2; % Max time to initiate movement
 Params.TimeoutReachTarget       = 1.6; % max time to reach reaching target
@@ -191,7 +191,7 @@ Params.UpTargetProbability      = 0.5; % for joystickTraining mode
 % Rewards
 Params.StdReward                = 120; %[ms]
 % multipliers for StdReward
-Params.UpReward                 = [rand(1,100)*0.8+0.1]; 
+Params.UpReward                 = [0.9 0.7 0.9 0.8 0.6 rand(1,100)*0.8+0.1]; 
 Params.DownReward               = 1.0-Params.UpReward;
 Params.UpReward=2*Params.UpReward;
 Params.DownReward=2*Params.DownReward;
