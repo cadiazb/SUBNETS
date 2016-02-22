c1900c4387d02019686b442b23f9113a3816825c
kderosier <kderosier6@gmail.com>
2016-02-22 14:33:50 -0800

    fixed typo

--- Task codes/EffortVSReward/TaskParams.m
+++ Task codes/EffortVSReward/TaskParams.m
@@ -116,7 +116,7 @@ Params.SessionCount = ct;
 
 Params.TrialTypeBlocks          = [3 5 ]; % sequence of trial types
 Params.LoopBlocks               = true; % if false, continue with last trial type forever
-Params.BlockLength              =3; %0; % number of successes per block
+Params.BlockLength              =30; % number of successes per block
 Params.NumTrials 				= 100000; % Choose a big number so task doesn't finish before hand
 
 % extend Params.TrialTypeBlocks to last until we reach NumTrials
